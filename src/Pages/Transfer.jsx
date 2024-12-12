014258833d8ebe7aa3879481563dc5016149ac5c
Raje <rajevictory@gmail.com>
2024-12-12 03:04:27 -0800

    new changes made

--- src/Pages/Transfer.jsx
+++ src/Pages/Transfer.jsx
@@ -8,9 +8,9 @@ export default function Transfer() {
     <div className='flex flex-col items-center py-10'>
         <h1 className='mb-5'>Transfer</h1>
         
-           <div className='flex gap-10 mb-6'>
-           <input className='border-b h-10' type="text" placeholder='Amount' />
-           <input className='border-b' type="text" placeholder='Etransfer Email' />
+           <div className='flex gap-10 mb-6 px-5'>
+           <input className=' h-10 md:w-[200px] w-full border-2 pl-2 border-gray-500 ' type="text" placeholder='Amount' />
+           <input className=' h-10 md:w-[200px] w-full border-2 pl-2 border-gray-500' type="text" placeholder='Etransfer Email' />
            </div>
     
      <li className='bg-blue-600 list-none text-white py-3 px-4 rounded-md'><NavLink to="/failed">Send</NavLink></li>
